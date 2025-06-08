4f9e79b36a04fa0651dd735ee0771bb23018d3eb
OmmDigi <ommdigitalsolution@gmail.com>
2025-06-08 13:33:31 +0530

    optimize 2

--- FRONTEND/app/tours/page.tsx
+++
@@ -1,9 +0,0 @@
-import PackagesListingPage from "@/components/Pages/PackagesListingPage";
-
-export default function page() {
-  return (
-    <div className="mt-16">
-      <PackagesListingPage key="Tours" category_slug="" current_page={1}/>
-    </div>
-  );
-}
