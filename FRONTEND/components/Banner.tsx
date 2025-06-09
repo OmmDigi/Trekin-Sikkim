9d37cc49071f7c0fb0606e1592580f7cd1c48f45
OmmDigi <ommdigitalsolution@gmail.com>
2025-06-09 13:13:13 +0530

    try to update

--- FRONTEND/components/Banner.tsx
+++ FRONTEND/components/Banner.tsx
@@ -138,41 +138,44 @@ function Banner() {
           );
         })}
 
-        {banner_info.map((item, index) => (
-          <div
-            key={item.id}
-            className={cn(
-              "absolute inset-0 bg-[#00000094] z-50",
-              currentBannerIndex === index
-                ? "opacity-100 blur-[0px] z-10"
-                : "opacity-0 blur-md z-0"
-            )}
-          >
-            <div className="max-w-3xl font-montserrat size-full mx-auto flex items-center justify-center flex-col space-y-5 mt-5 max-sm:max-w-[90%] max-sm:mt-0">
-              <div className="space-y-2">
-                <h2 className="text-4xl font-semibold text-white tracking-widest text-center max-sm:text-2xl">
-                  {item.title}
-                </h2>
-                <p className="text-center text-sm text-gray-100 tracking-widest max-sm:text-xs">
-                  {item.subtitle}
-                </p>
-              </div>
+        {banner_info.map((item, index) => {
+          const isLoaded = loaded.includes(index);
+          return isLoaded ? (
+            <div
+              key={item.id}
+              className={cn(
+                "absolute inset-0 bg-[#00000094] z-50",
+                currentBannerIndex === index
+                  ? "opacity-100 blur-[0px] z-10"
+                  : "opacity-0 blur-md z-0"
+              )}
+            >
+              <div className="max-w-3xl font-montserrat size-full mx-auto flex items-center justify-center flex-col space-y-5 mt-5 max-sm:max-w-[90%] max-sm:mt-0">
+                <div className="space-y-2">
+                  <h2 className="text-4xl font-semibold text-white tracking-widest text-center max-sm:text-2xl">
+                    {item.title}
+                  </h2>
+                  <p className="text-center text-sm text-gray-100 tracking-widest max-sm:text-xs">
+                    {item.subtitle}
+                  </p>
+                </div>
 
-              <div className="flex items-center gap-5">
-                <Link href="#our-packages-section">
-                  <Button className="min-w-[10rem] transition-all duration-1000 max-sm:min-[8rem] max-sm:pl-3 !bg-accent !text-white">
-                    <BiBookContent />
-                    Book Now
-                  </Button>
-                </Link>
+                <div className="flex items-center gap-5">
+                  <Link href="#our-packages-section">
+                    <Button className="min-w-[10rem] transition-all duration-1000 max-sm:min-[8rem] max-sm:pl-3 !bg-accent !text-white">
+                      <BiBookContent />
+                      Book Now
+                    </Button>
+                  </Link>
 
-                <div className=" border-secondary !bg-accent !text-white backdrop-blur-2xl size-10 flex items-center justify-center rounded-[50%] cursor-pointer max-sm:size-8">
-                  <IoIosPlay />
+                  <div className=" border-secondary !bg-accent !text-white backdrop-blur-2xl size-10 flex items-center justify-center rounded-[50%] cursor-pointer max-sm:size-8">
+                    <IoIosPlay />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ) : null;
+        })}
 
         <div className="absolute z-50 top-0 bottom-0 left-0 flex items-center pl-3 max-sm:hidden">
           <button
